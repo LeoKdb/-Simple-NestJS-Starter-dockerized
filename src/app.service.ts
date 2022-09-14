fc91f6af96b554ab30e6f8d2cb5483484a4c88fa
kidubo <innocentleonard0007@gmail.com>
2022-09-14 20:58:34 +0300

    second commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!, This is  Simple NestJS Starter dockerized';
+    return 'Hello World!, This is  Simple NestJS Starter dockerized Wow';
   }
 }
